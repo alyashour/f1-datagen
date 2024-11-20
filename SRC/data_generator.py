97c0afae80744459845450a0aa66a6a8872de804
Hamza Elkababji <hamza.kababji@gmail.com>
2024-11-19 20:33:10 -0500

    Added sample insert statements that we can use to shwo the database is working as intended (Not large amounts of tuples). Also Created a mini template for how data generation with faker can be done.

--- SRC/data_generator.py
+++ SRC/data_generator.py
@@ -1,1 +1,12 @@
 # Fake data generation can be completed here. We can use a library called faker if we agree on it.
+
+from faker import Faker #! We can use this library to generate fake data, just an idea; it's not necessary
+import csv
+
+fake = Faker()
+
+# with open("fake_data.csv", "w", newline="") as file:
+#     writer = csv.writer(file)
+#     writer.writerow(["Name", "Date of Birth", "Country"])
+#     for _ in range(100):
+#         writer.writerow([fake.name(), fake.date_of_birth(), fake.country()])
