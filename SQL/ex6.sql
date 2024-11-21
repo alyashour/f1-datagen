a488322e16d2ff032dbd16c1ba4b191796500713
Hamza Elkababji <hamza.kababji@gmail.com>
2024-11-21 01:32:52 -0500

    Added Select statements to show the changes caused by each query in Task 6.

--- SQL/ex6.sql
+++ SQL/ex6.sql
@@ -5,6 +5,9 @@ SELECT d.Driver_ID, c.Constructor_ID, '2023-01-01', 'Primary Driver'
 FROM Driver d, Constructor c
 WHERE d.Name = 'Lewis Hamilton' AND c.Name = 'Mercedes';
 
+-- Show the result of the insertion
+SELECT * FROM DriverEntry;
+
 -- Update several tuples at once
 -- Update total points for drivers with more than 10 race entries
 UPDATE Driver
@@ -16,8 +19,14 @@ WHERE Driver_ID IN (
     HAVING COUNT(RaceResult_ID) > 10
 );
 
+-- Show the result of the update
+SELECT * FROM Driver;
+
 -- Delete a set of tuples
 -- Delete all race results for Sebastian Vettel from Ferrari
 DELETE FROM RaceResult
 WHERE Driver_ID = (SELECT Driver_ID FROM Driver WHERE Name = 'Sebastian Vettel')
 AND Constructor_ID = (SELECT Constructor_ID FROM Constructor WHERE Name = 'Ferrari');
+
+-- Show the result of the deletion
+SELECT * FROM RaceResult;
