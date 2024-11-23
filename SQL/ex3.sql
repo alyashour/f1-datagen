7d1157030d530fa69e122a8de6ae14a073ac5662
Hamza Elkababji <hamza.kababji@gmail.com>
2024-11-23 00:03:06 -0500

    Completed SQL commands for ex5,6, and 7. Also updated pipfile to allow for 3.12.* versions of python

--- SQL/ex3.sql
+++ SQL/ex3.sql
@@ -2,19 +2,30 @@
 INSERT INTO Driver (Name, DOB, Gender, Country_of_Birth)
 VALUES ('Lewis Hamilton', '1985-01-07', 'M', 'United Kingdom');
 
-SELECT * FROM Driver;
+-- Select the last 10 rows, prioritizing the most recently inserted ones
+SELECT *
+FROM Driver
+ORDER BY Driver_ID DESC -- Assuming Driver_ID is an auto-increment column
+LIMIT 10;
 
 -- INSERT ... SELECT Command
 INSERT INTO Driver (Name, DOB, Gender, Country_of_Birth)
 SELECT 'Sebastian Vettel', '1987-07-03', 'M', 'Germany'
 FROM DUAL;
 
-SELECT * FROM Driver;
+-- Select the last 10 rows, prioritizing the most recently inserted ones
+SELECT *
+FROM Driver
+ORDER BY Driver_ID DESC
+LIMIT 10;
 
 -- INSERT ... ON DUPLICATE KEY UPDATE Command
 INSERT INTO Driver (Driver_ID, Name, DOB, Gender, Country_of_Birth)
-VALUES (1, 'Max Verstappen', '1997-09-30', 'M', 'Netherlands')
+VALUES (1, 'Max Verstappen', '1997-09-30', 'Male', 'Netherlands')
 ON DUPLICATE KEY UPDATE Name = 'Max Verstappen', DOB = '1997-09-30', Gender = 'M', Country_of_Birth = 'Netherlands';
 
--- Select all rows from Driver table to see the end result
-SELECT * FROM Driver;
+-- Select the last 10 rows, prioritizing the most recently inserted or updated ones
+SELECT *
+FROM Driver
+ORDER BY Driver_ID ASC
+LIMIT 10;
--- SQL/ex6.sql
+++ SQL/ex6.sql
@@ -1,15 +1,46 @@
 -- Insert the result of a query
 -- Insert a new driver entry for Lewis Hamilton into the Mercedes constructor
-INSERT INTO DriverEntry (Driver_ID, Constructor_ID, Start_Date, Driver_Role)
-SELECT d.Driver_ID, c.Constructor_ID, '2023-01-01', 'Primary Driver'
-FROM Driver d, Constructor c
-WHERE d.Name = 'Lewis Hamilton' AND c.Name = 'Mercedes';
 
--- Show the result of the insertion
-SELECT * FROM DriverEntry;
+-- Delete all previous entries of Lewis Hamilton in Mercedes
+DELETE FROM DriverEntry
+WHERE Driver_ID = (SELECT Driver_ID FROM Driver WHERE Name = 'Lewis Hamilton')
+AND Constructor_ID = (SELECT Constructor_ID FROM Constructor WHERE Name = 'Mercedes');
+
+-- Select to show the entries before inserting the new entry for Lewis Hamilton
+SELECT * FROM DriverEntry
+WHERE Driver_ID = (SELECT Driver_ID FROM Driver WHERE Name = 'Lewis Hamilton')
+AND Constructor_ID = (SELECT Constructor_ID FROM Constructor WHERE Name = 'Mercedes')
+LIMIT 10;
+
+-- Insert the new entry
+INSERT INTO DriverEntry (Driver_ID, Constructor_ID, Start_Date, Driver_Role, Season_ID)
+SELECT 
+    (SELECT Driver_ID FROM Driver WHERE Name = 'Lewis Hamilton'),
+    (SELECT Constructor_ID FROM Constructor WHERE Name = 'Mercedes'),
+    '2019-01-01',
+    'Primary Driver',
+    2019;
+
+-- Select to show the new entry for Lewis Hamilton
+SELECT * FROM DriverEntry
+WHERE Driver_ID = (SELECT Driver_ID FROM Driver WHERE Name = 'Lewis Hamilton')
+AND Constructor_ID = (SELECT Constructor_ID FROM Constructor WHERE Name = 'Mercedes')
+LIMIT 10;
 
 -- Update several tuples at once
 -- Update total points for drivers with more than 10 race entries
+
+-- Select to show the total points before the update
+SELECT * FROM Driver
+WHERE Driver_ID IN (
+    SELECT Driver_ID
+    FROM RaceResult
+    GROUP BY Driver_ID
+    HAVING COUNT(RaceResult_ID) > 10
+)
+LIMIT 10;
+
+-- Perform the update
 UPDATE Driver
 SET Total_Points = Total_Points + 10
 WHERE Driver_ID IN (
@@ -19,14 +50,35 @@ WHERE Driver_ID IN (
     HAVING COUNT(RaceResult_ID) > 10
 );
 
--- Show the result of the update
-SELECT * FROM Driver;
+-- Select to show the updated total points for drivers with more than 10 race entries
+SELECT * FROM Driver
+WHERE Driver_ID IN (
+    SELECT Driver_ID
+    FROM RaceResult
+    GROUP BY Driver_ID
+    HAVING COUNT(RaceResult_ID) > 10
+)
+LIMIT 10;
 
 -- Delete a set of tuples
--- Delete all race results for Sebastian Vettel from Ferrari
-DELETE FROM RaceResult
-WHERE Driver_ID = (SELECT Driver_ID FROM Driver WHERE Name = 'Sebastian Vettel')
-AND Constructor_ID = (SELECT Constructor_ID FROM Constructor WHERE Name = 'Ferrari');
+-- Delete all race results for a random driver where RaceResult_ID is even
 
--- Show the result of the deletion
-SELECT * FROM RaceResult;
+-- Select a random Driver_ID
+SET @randomDriverID = (SELECT Driver_ID FROM Driver ORDER BY RAND() LIMIT 1);
+
+-- Select to show the race results before the delete
+SELECT * FROM RaceResult
+WHERE Driver_ID = @randomDriverID
+AND MOD(RaceResult_ID, 2) = 0
+LIMIT 10;
+
+-- Perform the delete
+DELETE FROM RaceResult
+WHERE Driver_ID = @randomDriverID
+AND MOD(RaceResult_ID, 2) = 0;
+
+-- Select to show the remaining race results for the random driver where RaceResult_ID is even
+SELECT * FROM RaceResult
+WHERE Driver_ID = @randomDriverID
+AND MOD(RaceResult_ID, 2) = 0
+LIMIT 10;
